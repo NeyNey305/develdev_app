41d9ab06afdd2cea2637aab9deb155b454991b48
Jenee Washington <jeneewashington@icloud.com>
2017-01-09 22:08:46 -0500

    Implemented user authentication

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,14 @@
 Rails.application.routes.draw do
-  get 'welcome/index'
+  resources :projects
+  # root :to => 'welcrome#index'
   root 'welcome#index'
+  resources :user_sessions
+  resources :users
+
+  get 'login' => 'user_sessions#new', :as => :login
+  post 'logout' => 'user_sessions#destroy', :as => :logout
+
+  get 'welcome' => 'welcome#index', :as => :home
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
