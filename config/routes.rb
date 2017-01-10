dd1d55f49ca883a047d39bbbcff45d42c0045fb3
Jenee Washington <jeneewashington@icloud.com>
2017-01-10 18:30:10 -0500

    Draft of develdev homepage

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,17 @@
 Rails.application.routes.draw do
+  get 'dashboard/index'
+  # get 'dashboard' => 'dashboard#index', :as => :dashboard
+
+  get 'home/index'
+
   resources :projects
-  # root :to => 'welcrome#index'
-  root 'welcome#index'
+
   resources :user_sessions
   resources :users
 
   get 'login' => 'user_sessions#new', :as => :login
   post 'logout' => 'user_sessions#destroy', :as => :logout
 
-  get 'welcome' => 'welcome#index', :as => :home
-
+  root 'home#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
